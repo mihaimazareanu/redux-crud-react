491410bb756724892ad8ff36061fb1bc7649fb93
mihaimazareanu <49844133+mihaimazareanu@users.noreply.github.com>
2023-01-24 22:33:28 +0100

    upload project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-
 import usersReducer from "./features/Users";
 
 const store = configureStore({
